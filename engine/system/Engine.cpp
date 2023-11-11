8af1f0712568aae0bf835f1552fd189136252991
Adam Flett <adam@adamflett.net>
2023-11-11 17:34:09 +0800

    SDL gl rendering

--- engine/system/Engine.cpp
+++ engine/system/Engine.cpp
@@ -6,8 +6,13 @@ namespace ige {
 Engine::Engine() {
     SDL_Init(SDL_INIT_EVERYTHING);
 
-    std::cout << "Starting Application..."
+    std::cout << "Starting Engine..."
               << "\n";
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
+                        SDL_GL_CONTEXT_PROFILE_CORE);
 
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
         std::cerr << "Failed to initialize SDL: " << SDL_GetError()
@@ -15,7 +20,8 @@ Engine::Engine() {
     }
 
     m_window = SDL_CreateWindow("SDLGL Test", 800, 600,
-                                SDL_WindowFlags::SDL_WINDOW_RESIZABLE);
+                                SDL_WindowFlags::SDL_WINDOW_OPENGL |
+                                    SDL_WindowFlags::SDL_WINDOW_RESIZABLE);
     if (!m_window) {
         std::cerr << "Failed to create window: " << SDL_GetError() << std::endl;
         SDL_Quit();
@@ -28,6 +34,16 @@ Engine::Engine() {
                   << std::endl;
         SDL_DestroyWindow(m_window);
         SDL_Quit();
+    }
+
+    auto m_glContext = SDL_GL_CreateContext(m_window);
+    if (!m_glContext) {
+        std::cerr << "Failed to create OpenGL context: " << SDL_GetError()
+                  << std::endl;
+        SDL_DestroyRenderer(m_renderer);
+        SDL_DestroyWindow(m_window);
+        SDL_Quit();
+        // Handle OpenGL context creation failure
     }
 
     SDL_Event event;
@@ -45,10 +61,19 @@ Engine::Engine() {
             }
         }
 
-        SDL_SetRenderDrawColor(m_renderer, 100, 149, 237, 255);
-        SDL_RenderClear(m_renderer);
-        SDL_RenderPresent(m_renderer);
+        glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Your rendering code here
+
+        SDL_GL_SwapWindow(m_window);
+
+        // SDL_SetRenderDrawColor(m_renderer, 100, 149, 237, 255);
+        // SDL_RenderClear(m_renderer);
+        // SDL_RenderPresent(m_renderer);
     }
+
+    SDL_GL_DeleteContext(m_glContext);
 }
 Engine::~Engine() {
     SDL_DestroyRenderer(m_renderer);
